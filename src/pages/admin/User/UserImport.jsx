72a9cdcecc3327b7a171443eb7bb2d38ba6719f3
pthienquan1 <quan01011997123@gmail.com>
2023-05-13 11:11:11 +0700

    Read data from file xlsx and fill this data to Tablein modal of UserImport

--- src/pages/admin/User/UserImport.jsx
+++ src/pages/admin/User/UserImport.jsx
@@ -13,7 +13,16 @@ import {
   Divider,
 } from "antd";
 
-const { Dragger } = Upload;
+import * as XLSX from 'xlsx';
+
+
+const UserImport = (props) => {
+
+  const [dataExcel, setDataExcel] = useState([]);
+  const { openModalCreateImport, setOpenModalCreateImport } = props;
+  const [isSubmitImport, setIsSubmitImport] = useState(false);
+
+  const { Dragger } = Upload;
 
 const dummyRequest = ({ file, onSuccess }) => {
   setTimeout(() => {
@@ -26,7 +35,7 @@ const propsUpload = {
   maxCount: 1,
   // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
   accept:
-    ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel",
+    ".csv, .xls,.xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel",
   customRequest: dummyRequest,
 
   onChange(info) {
@@ -35,6 +44,21 @@ const propsUpload = {
       console.log(info.file, info.fileList);
     }
     if (status === "done") {
+      if(info.fileList && info.fileList.length > 0){
+        const file = info.fileList[0].originFileObj;
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function (e) {
+          const data = new Uint8Array(reader.result);
+          const workbook = XLSX.read(data, {type:'array'})
+          const sheet = workbook.Sheets[workbook.SheetNames[0]];
+          const json = XLSX.utils.sheet_to_json(sheet, {
+            header: ["fullName", "email", "phone"],
+            range: 1
+          });
+          if(json && json.length > 0) setDataExcel(json);
+        }
+      }
       message.success(`${info.file.name} file uploaded successfully.`);
     } else if (status === "error") {
       message.error(`${info.file.name} file upload failed.`);
@@ -44,16 +68,12 @@ const propsUpload = {
     console.log("Dropped files", e.dataTransfer.files);
   },
 };
-
-const UserImport = (props) => {
-  const { openModalCreateImport, setOpenModalCreateImport } = props;
-  const [isSubmitImport, setIsSubmitImport] = useState(false);
   return (
     <div>
       <Modal
         title="Import user"
         open={openModalCreateImport}
-        
+        onOk={() => setOpenModalCreateImport(false)}
         onText={"Tạo mới"}
         cancelText={"Hủy"}
         onCancel={() => setOpenModalCreateImport(false)}
@@ -73,6 +93,7 @@ const UserImport = (props) => {
         </Dragger>
         <div style={{ paddingTop: 20 }}>
           <Table
+          dataSource={dataExcel}
             title={() => <span>Dữ liệu upload</span>}
             columns={[
               { dataIndex: "fullName", title: "Tên hiển thị" },
